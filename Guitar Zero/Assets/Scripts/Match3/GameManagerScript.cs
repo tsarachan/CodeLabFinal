2541ea08c07ee537de9730c2258f606a0ec32e7f
tsarachan <tes353@nyu.edu>
2016-12-04 17:19:05 -0500

    Basic functionality restored
    Tokens display reasonably, swap, match, and new tokens appear and fall.

---
+++ Guitar Zero/Assets/Scripts/Match3/GameManagerScript.cs
@@ -0,0 +1,232 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManagerScript : MonoBehaviour {
+
+	//width and height of the token grid, in tokens
+	public int gridWidth = 8;
+	public int gridHeight = 8;
+
+
+	//used to reposition the tokens visually
+	public float tokenSize = 1.0f;
+	public float xOffset = 0.5f;
+	public float yOffset = 0.0f;
+
+
+	//other scripts fundamental to the game's operation
+	protected MatchManagerScript matchManager;
+	protected InputManagerScript inputManager;
+	protected RepopulateScript repopulateManager;
+	protected MoveTokensScript moveTokenManager;
+
+	//parent object for all tokens
+	public GameObject grid;
+
+	//array of tokens used for game-state management
+	public GameObject[,] gridArray;
+
+	protected UnityEngine.Object[] tokenTypes; //used to load tokens into the game
+	protected Sprite[] noteSprites; //used to reassign sprites when there would otherwise be a start-of-game match
+
+	//the sprite used for the strings in the background
+	protected GameObject stringGraphic;
+
+
+
+	public virtual void Start () {
+		tokenTypes = (UnityEngine.Object[])Resources.LoadAll("Tokens/");
+		noteSprites = Resources.LoadAll<Sprite>("Sprites/Note sprites");
+		gridArray = new GameObject[gridWidth, gridHeight];
+		matchManager = GetComponent<MatchManagerScript>();
+		inputManager = GetComponent<InputManagerScript>();
+		repopulateManager = GetComponent<RepopulateScript>();
+		moveTokenManager = GetComponent<MoveTokensScript>();
+		stringGraphic = Resources.Load("String") as GameObject;
+		MakeGrid();
+		ChangeGridDuplicates();
+	}
+
+	public virtual void Update(){
+		//every frame, check whether the grid is full of tokens.
+
+		if(!GridHasEmpty()){
+			//if the grid is full of tokens and has matches, remove them.
+			if(matchManager.GridHasMatch()){
+				matchManager.RemoveMatches();
+			} else {
+				//if the grid is full and there are no matches, wait for the player to make a move (and look for it in InputManager)
+				inputManager.SelectToken();
+			}
+
+		} else {
+			if(!moveTokenManager.move){
+				//if the icons are currently moving, set them up to move and leave it be
+				moveTokenManager.SetupTokenMove();
+			}
+			if(!moveTokenManager.MoveTokensToFillEmptySpaces()){
+				//if the MoveTokenManager hasn't added any tokens to the grid
+				//tell Repopulate Script to add new tokens
+				repopulateManager.AddNewTokensToRepopulateGrid();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Create a parent object for tokens, and then create a token at each grid position
+	/// </summary>
+	void MakeGrid(){
+		grid = new GameObject("TokenGrid");
+
+		for(int x = 0; x < gridWidth; x++){
+
+//			GameObject newStringGraphic = Instantiate(stringGraphic,
+//				new Vector3(x - gridWidth/2 * tokenSize,
+//					0.0f,
+//					0.0f),
+//				Quaternion.identity) as GameObject;
+//			newStringGraphic.transform.parent = transform.root.Find("Strings");
+
+			for(int y = 0; y < gridHeight; y++){
+				AddTokenToPosInGrid(x, y, grid);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Determine whether there are any empty spaces in the grid.
+	/// </summary>
+	/// <returns><c>true</c> upon finding an empty space, <c>false</c> if it finds none.</returns>
+	public virtual bool GridHasEmpty(){
+		//this checks every x and y in the grid
+		for(int x = 0; x < gridWidth; x++){
+			for(int y = 0; y < gridHeight ; y++){
+				if(gridArray[x, y] == null){
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/// <summary>
+	/// Gets the grid position of a specific token. (0, 0) is the lower-left.
+	/// 
+	/// This is not the world position of the token--that is handled by GetWorldPositionFromGridPosition(), below.
+	/// </summary>
+	/// 
+	/// <param name="token">The token whose grid position is to be found,</param>
+	/// <returns>The coordinates of the token in the grid.</returns>
+	public Vector2 GetPositionOfTokenInGrid(GameObject token){
+		for(int x = 0; x < gridWidth; x++){
+			for(int y = 0; y < gridHeight ; y++){
+				if(gridArray[x, y] == token){
+					return(new Vector2(x, y));
+				}
+			}
+		}
+		return new Vector2();
+	}
+
+
+	/// <summary>
+	/// Converts a grid position into a world space position.
+	/// </summary>
+	/// 
+	/// <param name="x">The grid position x-coordinate</param>
+	/// <param name="y">The grid position y-coordinate</param>
+	/// <returns>The grid position's 2D world space position</returns>
+	public Vector2 GetWorldPositionFromGridPosition(int x, int y){
+		return new Vector2(
+			(x - gridWidth/2) * tokenSize + xOffset,
+			(y - gridHeight/2) * tokenSize + yOffset);
+	}
+
+
+	/// <summary>
+	/// This creates a random token and puts it into the grid at the given coordinate
+	/// (It also makes the token a child of the grid, to keep the hierarchy tidy.)
+	/// </summary>
+	/// 
+	/// <param name="x">An int x that is the x coordinate in the grid</param>
+	/// <param name="y">An int y that is the y coordinate in the grid</param>
+	/// <param name="parent">The parent of this token. It should be TokenGrid</param> 
+	public void AddTokenToPosInGrid(int x, int y, GameObject parent){
+		Vector3 position = GetWorldPositionFromGridPosition(x, y);
+		GameObject token = 
+			//we create a random kind of token, at that exact position in the grid, with the same rotation as its parent (TokenGrid)
+			Instantiate(GetTokenForColumn(x), 
+				position, 
+				Quaternion.identity) as GameObject;
+		token.transform.parent = parent.transform;
+		//then, we put this token into the array of tokens
+		gridArray[x, y] = token;
+	}
+
+	/// <summary>
+	/// This is called at the start of the game to see if the random process for creating tokens has left matches. If so, it changes
+	/// the affected sprites to try to eliminate matches, and then is called again recursively until no matches are detected.
+	/// </summary>
+	protected void ChangeGridDuplicates(){
+		bool foundDuplicates = false;
+
+		for (int x = 0; x < gridWidth; x++){
+			for (int y = 0; y < gridHeight; y++){
+				if (x < gridWidth - 2){
+					if (matchManager.GridHasAChord(x, y)){
+						gridArray[x, y].GetComponent<SpriteRenderer>().sprite = ChangeSprite(x, y);
+						foundDuplicates = true;
+					}
+				}
+
+				if (x < gridWidth - 3 && y < gridHeight - 2){
+					if (matchManager.GridHasCChord(x, y)){
+						gridArray[x, y].GetComponent<SpriteRenderer>().sprite = ChangeSprite(x, y);
+						foundDuplicates = true;
+					}
+				}
+
+				if (x < gridWidth - 5 && y < gridHeight - 1){
+					if (matchManager.GridHasGChord(x, y)){
+						gridArray[x, y].GetComponent<SpriteRenderer>().sprite = ChangeSprite(x, y);
+						foundDuplicates = true;
+					}
+				}
+			}
+		}
+
+		if (foundDuplicates) { ChangeGridDuplicates(); }
+	}
+
+	protected Sprite ChangeSprite(int x, int y){
+		int index = Array.IndexOf(noteSprites, gridArray[x, y].GetComponent<SpriteRenderer>().sprite);
+		index++;
+		if (index > noteSprites.Length - 1) { index = 0; }
+		return noteSprites[index];
+	}
+
+	/// <summary>
+	/// Returns a token appropriate for the column provided.
+	/// 
+	/// This assumes that tokens are named with the number of the columns they can appear in!
+	/// The number must be zero-indexed, so the strings are 0-5, not 1-6.
+	/// </summary>
+	/// <returns>A token that can exist in the column.</returns>
+	/// <param name="column">The column to be filled.</param>
+	protected GameObject GetTokenForColumn(int column){
+		List<GameObject> tokens = new List<GameObject>();
+
+		foreach (GameObject token in tokenTypes){
+			if (token.name.Contains(column.ToString())){
+				tokens.Add(token);
+			}
+		}
+
+		return tokens[UnityEngine.Random.Range(0, tokens.Count)];
+	}
+}
---
+++ Guitar Zero/Assets/Scripts/Match3/MatchManagerScript.cs
@@ -0,0 +1,354 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchManagerScript : MonoBehaviour {
+
+	protected GameManagerScript gameManager;    //"protected" means this field is public to child scripts
+	//but not to unrelated scripts
+
+	protected GameObject colorBurst;
+	const int CHORD_SIZE = 3;
+	protected int crowdMultiplier = 1;
+	public int CrowdMultiplier{
+		get { return crowdMultiplier; }
+		set{
+			crowdMultiplier = value;
+		}
+	}
+	private int successesThisMultiplier = 0;
+	public int SuccessesThisMultiplier{
+		get { return successesThisMultiplier; }
+		set{
+			successesThisMultiplier = value;
+		}
+	}
+	protected const int MIN_CROWD_MULTIPLIER = 1;
+	public int Min_Crowd_Multiplier{
+		get { return MIN_CROWD_MULTIPLIER; }
+	}
+	protected const int A_CHORD_MULTIPLIER = 1;
+	protected const int C_CHORD_MULTIPLIER = 3;
+	protected const int G_CHORD_MULTIPLIER = 2;
+	protected const int D_CHORD_MULTIPLIER = 4;
+	protected const int F_CHORD_MULTIPLIER = 6;
+	protected const string SCORE_CANVAS = "Score canvas";
+	protected const string SCORE_TEXT = "Score";
+	protected const string CROWD_METER = "Crowd anger meter";
+	protected const string COLOR_BURST = "Color burst";
+
+	protected const string A_CHORD_NAME = "A";
+	protected const string C_CHORD_NAME = "C";
+	protected const string G_CHORD_NAME = "G";
+	protected const string D_CHORD_NAME = "D";
+	protected const string F_CHORD_NAME = "F";
+
+
+	public virtual void Awake (){
+		gameManager = GetComponent<GameManagerScript>();
+		colorBurst = Resources.Load(COLOR_BURST) as GameObject;
+	}
+
+	/// <summary>
+	/// Checks the entire grid for matches.
+	/// </summary>
+	/// 
+	/// <returns><c>true</c> if there are any matches, <c>false</c> otherwise.</returns>
+	public virtual bool GridHasMatch(){
+		bool match = false; //assume there is no match
+
+		//check each square in the grid
+		for(int x = 0; x < gameManager.gridWidth; x++){
+			for(int y = 0; y < gameManager.gridHeight ; y++){
+				if (x < gameManager.gridWidth - 2){	//GridHasHorizontalMatch checks 2 to the right
+					//gameManager.gridWidth - 2 ensures you're never extending into
+					//a space that doesn't exist
+					match = match || GridHasAChord(x, y); //if match was ever set to true, it stays true forever
+				}
+
+				if (x < gameManager.gridWidth - 3 && y < gameManager.gridHeight - 2){
+					match = match || GridHasCChord(x, y);
+				}
+
+				if (x < gameManager.gridWidth - 5 && y < gameManager.gridHeight - 1){
+					match = match || GridHasGChord(x, y);
+				}
+
+				if (x < gameManager.gridWidth - 2 && y < gameManager.gridHeight - 1 && y > 0) {
+					match = match || GridHasDChord(x, y);
+				}
+
+				//				if (x < gameManager.gridWidth - 5 &&  y > 1) {
+				//					match = match || GridHasFChord(x, y);
+				//				}
+			}
+		}
+
+		return match;
+	}
+
+
+	#region chord determination
+
+	/*
+	 * 
+	 * These check whether there's a match, according to the layout of the tokens.
+	 * These don't check where the layout appears; that's handled by GameManagerScript, which doesn't
+	 * allow tokens to appear in the wrong columns.
+	 * 
+	 * These also check to make sure that the sprite has the correct name. Otherwise, the layouts would be
+	 * chord-agnostic; one would be able to score by making the "A" patterns out of Cs, for example.
+	 * For this to work, the sprites must have the chord name as a capital letter, and no sprite can
+	 * have another chord's letter as a capital!
+	 * 
+	 */
+
+	public bool GridHasAChord(int x, int y){
+		GameObject token1 = gameManager.gridArray[x + 0, y + 0];
+		GameObject token2 = gameManager.gridArray[x + 1, y + 0];
+		GameObject token3 = gameManager.gridArray[x + 2, y + 0];
+
+		if(token1 != null && token2 != null && token3 != null){ //ensure all of the tokens exist
+			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+
+			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite
+				&& sr1.sprite.name.Contains(A_CHORD_NAME));  //compare their sprites
+		} else {
+			return false;
+		}
+	}
+
+	public bool GridHasCChord(int x, int y){
+		GameObject token1 = gameManager.gridArray[x + 0, y + 0];
+		GameObject token2 = gameManager.gridArray[x + 1, y + 1];
+		GameObject token3 = gameManager.gridArray[x + 3, y + 2];
+
+		if(token1 != null && token2 != null && token3 != null){
+			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+
+			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite
+				&& sr1.sprite.name.Contains(C_CHORD_NAME));  //compare their sprites
+		} else {
+			return false;
+		}
+	}
+
+	public bool GridHasGChord(int x, int y){
+		GameObject token1 = gameManager.gridArray[x + 0, y + 0];
+		GameObject token2 = gameManager.gridArray[x + 1, y + 1];
+		GameObject token3 = gameManager.gridArray[x + 5, y + 0];
+
+		if(token1 != null && token2 != null && token3 != null){
+			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+
+			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite
+				&& sr1.sprite.name.Contains(G_CHORD_NAME));  //compare their sprites
+		} else {
+			return false;
+		}
+	}
+
+	public bool GridHasDChord(int x, int y){
+		GameObject token1 = gameManager.gridArray[x + 0, y + 0];
+		GameObject token2 = gameManager.gridArray[x + 1, y - 1];
+		GameObject token3 = gameManager.gridArray[x + 2, y + 0];
+
+		if(token1 != null && token2 != null && token3 != null){
+			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+
+			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite
+				&& sr1.sprite.name.Contains(D_CHORD_NAME));  //compare their sprites
+		} else {
+			return false;
+		}
+	}
+
+	//	public bool GridHasFChord(int x, int y){
+	//		GameObject token1 = gameManager.gridArray[x + 0, y + 0];
+	//		GameObject token2 = gameManager.gridArray[x + 1, y - 2];
+	//		GameObject token3 = gameManager.gridArray[x + 2, y - 2];
+	//		GameObject token4 = gameManager.gridArray[x + 3, y - 1];
+	//		GameObject token5 = gameManager.gridArray[x + 4, y + 0];
+	//		GameObject token6 = gameManager.gridArray[x + 5, y + 0];
+	//
+	//		if(token1 != null && token2 != null && token3 != null
+	//			&& token4 != null && token5 != null && token6 != null){
+	//			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+	//			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+	//			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+	//			SpriteRenderer sr4 = token4.GetComponent<SpriteRenderer>();
+	//			SpriteRenderer sr5 = token5.GetComponent<SpriteRenderer>();
+	//			SpriteRenderer sr6 = token6.GetComponent<SpriteRenderer>();
+	//
+	//			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite
+	//					&& sr3.sprite == sr4.sprite && sr4.sprite == sr5.sprite
+	//					&& sr5.sprite == sr6.sprite
+	//					&& sr1.sprite.name.Contains(F_CHORD_NAME));  //compare their sprites to see if they're the same
+	//		} else {
+	//			return false;
+	//		}
+	//	}
+
+	#endregion
+
+	/// <summary>
+	/// Destroys all tokens in a match of three or more
+	/// </summary>
+	/// <returns>The number of tokens destroyed.</returns>
+	public virtual int RemoveMatches(){
+		int numRemoved = 0;
+
+		//iterate across entire grid, looking for matches
+		//wherever a chord is found, respond
+		for(int x = 0; x < gameManager.gridWidth; x++){
+			for(int y = 0; y < gameManager.gridHeight ; y++){
+
+				if (x < gameManager.gridWidth - 2){
+					if (GridHasAChord(x, y)){
+						RemoveChord(x, y, 'A');
+						numRemoved += 1;
+					}
+				}
+
+				if (x < gameManager.gridWidth - 3 && y < gameManager.gridHeight - 2){
+					if (GridHasCChord(x, y)){
+						RemoveChord(x, y, 'C');
+						numRemoved += 1;
+					}
+				}
+
+				if (x < gameManager.gridWidth - 5 && y < gameManager.gridHeight - 1){
+					if (GridHasGChord(x, y)){
+						RemoveChord(x, y, 'G');
+						numRemoved += 1;
+					}
+				}
+
+				if (x < gameManager.gridWidth - 2 && y < gameManager.gridHeight - 1 && y > 0){
+					if (GridHasDChord(x, y)){
+						RemoveChord(x, y, 'D');
+						numRemoved+= 1;
+					}
+				}
+
+				//				if (x < gameManager.gridWidth - 5 &&  y > 1){
+				//					if (GridHasFChord(x, y)){
+				//						RemoveChord(x, y, 'F');
+				//						numRemoved += 1;
+				//					}
+				//				}
+			}
+		}
+
+		return numRemoved;
+	}
+
+	protected void RemoveChord(int x, int y, char chord){
+		GameObject token1 = gameManager.gridArray[x, y];
+		gameManager.gridArray[x, y] = null;
+
+
+		GameObject token2 = null;
+		GameObject token3 = null;
+
+		switch(chord){
+		case 'A':
+			token2 = gameManager.gridArray[x + 1, y + 0];
+			gameManager.gridArray[x + 1, y + 0] = null;
+			token3 = gameManager.gridArray[x + 2, y + 0];
+			gameManager.gridArray[x + 2, y + 0] = null;
+			break;
+		case 'C':
+			token2 = gameManager.gridArray[x + 1, y + 1];
+			gameManager.gridArray[x + 1, y + 1] = null;
+			token3 = gameManager.gridArray[x + 3, y + 2];
+			gameManager.gridArray[x + 3, y + 2] = null;
+			break;
+		case 'G':
+			token2 = gameManager.gridArray[x + 1, y + 1];
+			gameManager.gridArray[x + 1, y + 1] = null;
+			token3 = gameManager.gridArray[x + 5, y + 0];
+			gameManager.gridArray[x + 5, y + 0] = null;
+			break;
+		case 'D':
+			token2 = gameManager.gridArray[x + 1, y - 1];
+			gameManager.gridArray[x + 1, y - 1] = null;
+			token3 = gameManager.gridArray[x + 2, y + 0];
+			gameManager.gridArray[x + 2, y + 0] = null;
+			break;
+			//		case 'F':
+			//				token2 = gameManager.gridArray[x + 1, y - 2];
+			//				gameManager.gridArray[x + 1, y - 2] = null;
+			//				token3 = gameManager.gridArray[x + 2, y - 2];
+			//				gameManager.gridArray[x + 2, y - 2] = null;
+			//
+			//				//the F chord has 6 notes, so handle the first 3 normally, and the remaining 3 here
+			//				GameObject token4 = gameManager.gridArray[x + 3, y - 1];
+			//				gameManager.gridArray[x + 3, y - 1] = null;
+			//				GameObject token5 = gameManager.gridArray[x + 4, y + 0];
+			//				gameManager.gridArray[x + 4, y + 0] = null;
+			//				GameObject token6 = gameManager.gridArray[x + 5, y + 0];
+			//				gameManager.gridArray[x + 5, y + 0] = null;
+			//
+			//				ChordFeedback(new GameObject[] { token4, token5, token6 }, chord);
+			//
+			//				Destroy(token4);
+			//				Destroy(token5);
+			//				Destroy(token6);
+			//				
+			//				break;
+		default:
+			Debug.Log("Illegal chord: " + chord);
+			break;
+		}
+
+		ChordFeedback(new GameObject[] { token1, token2, token3 }, chord);
+
+		Destroy(token1);
+		Destroy(token2);
+		Destroy(token3);
+
+		SuccessesThisMultiplier++;
+
+		if (SuccessesThisMultiplier >= CrowdMultiplier){
+			CrowdMultiplier++;
+			SuccessesThisMultiplier = 0;
+		}
+	}
+
+	protected void ChordFeedback(GameObject[] tokens, char chord){
+		int chordMultiplier = 1;
+
+		switch (chord){
+		case 'A':
+			chordMultiplier = A_CHORD_MULTIPLIER;
+			break;
+		case 'C':
+			chordMultiplier = C_CHORD_MULTIPLIER;
+			break;
+		case 'G':
+			chordMultiplier = G_CHORD_MULTIPLIER;
+			break;
+		case 'D':
+			chordMultiplier = D_CHORD_MULTIPLIER;
+			break;
+			//			case 'F':
+			//				chordMultiplier = F_CHORD_MULTIPLIER;
+			//				break;
+		default:
+			Debug.Log("Illegal chord: " + chord);
+			break;
+		}
+
+		foreach (GameObject token in tokens){
+			//Instantiate(colorBurst, token.transform.position, Quaternion.identity);
+		}
+	}
+}
