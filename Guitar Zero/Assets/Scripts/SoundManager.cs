cf620057901521c71add1c3124433edf13a43bbd
tsarachan <tes353@nyu.edu>
2016-12-09 19:19:47 -0500

    Adding sound control
    Adding functionality to control relative volume of different sounds.

---
+++ Guitar Zero/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioMixer mixer;
+	private AudioSource audioSource;
+
+
+	private void Start(){
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void SetMusicVolume(float newVol){
+		mixer.SetFloat("music_volume", newVol);
+	}
+}
